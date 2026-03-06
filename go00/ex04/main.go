07d9ac125343467df61fc550270f1f9389ec0750
Johannes Niedens <johannesniedens@gmail.com>
2026-03-06 13:36:48 -0300

    code(go00): ex04 finished - celcius to fahrenheit

---
+++ go00/ex04/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func errorPrint(s string) {
+	fmt.Fprintln(os.Stderr, s)
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		errorPrint("usage: ./ex04 <celsiu>\ngo run maing.go <celsius>")
+	}
+
+	celsius, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		errorPrint("Unable to parse Number. Type in any floating number")
+	}
+	fahrenheit := (celsius * 9 / 5) + 32
+	fmt.Printf("%0.2f° Celcius is %0.2f° Fahrenheit\n", celsius, fahrenheit)
+}
