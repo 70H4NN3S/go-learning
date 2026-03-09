350920b6fafc3afa97d70d6cb41eee24fd041b88
Johannes Niedens <johannesniedens@gmail.com>
2026-03-09 11:57:43 -0300

    feat(go02_ex01): Matrix Ops - Mock Data creation added in New Matrix

--- go02/ex01/matrix.go
+++ go02/ex01/matrix.go
@@ -4,6 +4,11 @@ func NewMatrix(rows, cols int) [][]float64 {
 	m := make([][]float64, rows)
 	for i := range m {
 		m[i] = make([]float64, cols)
+
+		// Fill with data to test
+		for j := range m[i] {
+			m[i][j] = float64((i*j + i + j) % 10)
+		}
 	}
 	return m
 }
