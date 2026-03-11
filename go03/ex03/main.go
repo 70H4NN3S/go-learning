2653d1f6a28caa466be5b95a581458ebe5251962
Johannes Niedens <johannesniedens@gmail.com>
2026-03-11 14:56:38 -0300

    feat(BinaryTree): added a test case with the main function

---
+++ go03/ex03/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	bST := BST{nil}
+	bST.Insert(5)
+	bST.Insert(3)
+	bST.Insert(1)
+	bST.Insert(2)
+	bST.Insert(4)
+	bST.Insert(6)
+	bST.Insert(8)
+	bST.Insert(7)
+
+	fmt.Println(bST.ToSlice())
+}
