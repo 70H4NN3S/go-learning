3173c8cb7670c4c200092200da7a0dfa8f951aed
Johannes Niedens <johannesniedens@gmail.com>
2026-03-10 23:15:02 -0300

    feat(AnagramGrouper): Added the anagram creator

--- go02/main.go
+++ go02/main.go
@@ -10,7 +10,10 @@ func main() {
 func GroupAnagrams(words []string) map[string][]string {
 	anagram := make(map[string][]string)
 	for _, word := range words {
+		soredWord := sortStrings(word)
+		anagram[soredWord] = append(anagram[soredWord], word)
 	}
+	return anagram
 }
 
 func sortStrings(s string) string {
