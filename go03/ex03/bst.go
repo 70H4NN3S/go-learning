b1595eb9e90804a78dba083913224b01aa5639b4
Johannes Niedens <johannesniedens@gmail.com>
2026-03-11 13:39:09 -0300

    feat(BinaryTree): added a simple Insert for the BST, balancing is needed

--- go03/ex03/bst.go
+++ go03/ex03/bst.go
@@ -9,3 +9,32 @@ type TreeNode struct {
 type BST struct {
 	Root *TreeNode
 }
+
+func (b *BST) Insert(val int) {
+	if val == b.Root.Val {
+		return
+	}
+	if b.Root == nil {
+		b.Root = &TreeNode{val, nil, nil}
+	}
+
+	for n := b.Root; true; {
+		if n.Val == val {
+			break
+		}
+		if n.Val > val {
+			if n.Left == nil {
+				n.Left = &TreeNode{val, nil, nil}
+				break
+			}
+			n = n.Left
+		}
+		if n.Val < val {
+			if n.Right == nil {
+				n.Right = &TreeNode{val, nil, nil}
+				break
+			}
+			n = n.Right
+		}
+	}
+}
