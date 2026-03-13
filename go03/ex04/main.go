065c3d2a9820c1c8c946b0e373fc71ab02bd2552
Johannes Niedens <johannesniedens@gmail.com>
2026-03-13 16:23:36 -0300

    feat(PointerPuzzle): doubleRet(n int) int added, returns a value

--- go03/ex04/main.go
+++ go03/ex04/main.go
@@ -7,8 +7,14 @@ func main() {
 	fmt.Println("Before changes:", i1)
 	doublePtr(&i1)
 	fmt.Println("After first call:", i1)
+	i1 = doubleRet(i1)
+	fmt.Println("After second change", i1)
 }
 
 func doublePtr(n *int) {
 	*n *= 2
+}
+
+func doubleRet(n int) int {
+	return n * 2
 }
