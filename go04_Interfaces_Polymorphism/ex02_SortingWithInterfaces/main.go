8e39eccdbdcb90f74b8e090e2c3a346158ec5af5
Johannes Niedens <johannesniedens@gmail.com>
2026-03-16 16:42:14 -0300

    feat(SortingWithInterfaces): added a second type called Products to recap and test everything

--- go04_Interfaces_Polymorphism/ex02_SortingWithInterfaces/main.go
+++ go04_Interfaces_Polymorphism/ex02_SortingWithInterfaces/main.go
@@ -19,6 +19,19 @@ func main() {
 	fmt.Println("len:", People(people).Len())
 	sort.Sort(People(people))
 	fmt.Println("Sorted:", people)
+
+	fmt.Println("\nPRODUCTS")
+
+	products := []Product{
+		{"Milk", 0.95, 20},
+		{"Meat", 3.99, 10},
+		{"Macbook Air M4", 999.99, 3},
+		{"Bag of Pens", 3, 12},
+	}
+	fmt.Println(products)
+	fmt.Println(Products(products).Len())
+	sort.Sort(Products(products))
+	fmt.Println(products)
 }
 
 type Person struct {
@@ -31,3 +44,15 @@ type People []Person
 func (p People) Len() int           { return len(p) }
 func (p People) Less(i, j int) bool { return p[i].Age < p[j].Age }
 func (p People) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+type Product struct {
+	Name  string
+	Price float64
+	Stock int
+}
+
+type Products []Product
+
+func (p Products) Len() int           { return len(p) }
+func (p Products) Less(i, j int) bool { return p[i].Price < p[j].Price }
+func (p Products) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
