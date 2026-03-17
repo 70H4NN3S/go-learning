1c7e8f5e7426b793686713db3bca032f308655bb
Johannes Niedens <johannesniedens@gmail.com>
2026-03-16 21:22:11 -0300

    feat(IOReaderPipeline): created the ROT13 rotation reader pipeline

--- go04_Interfaces_Polymorphism/ex03_IOReaderPipeline/pipeline.go
+++ go04_Interfaces_Polymorphism/ex03_IOReaderPipeline/pipeline.go
@@ -17,3 +17,26 @@ func (u *UpperReader) Read(p []byte) (n int, err error) {
 	}
 	return
 }
+
+type ROT13Reader struct {
+	inner io.Reader
+}
+
+func rot13byte(b byte) byte {
+	switch {
+	case b >= 'A' && b <= 'Z':
+		return 'A' + (b-'A'+13)%26
+	case b >= 'a' && b <= 'z':
+		return 'z' + (b-'z'+13)%26
+	default:
+		return b
+	}
+}
+
+func (r ROT13Reader) Read(p []byte) (n int, err error) {
+	n, err = r.inner.Read(p)
+	for i := 0; i < n; i++ {
+		p[i] = rot13byte(p[i])
+	}
+	return n, err
+}
