b2fc1cf0449d9801e163002b4e5fc4f070fdec71
Johannes Niedens <johannesniedens@gmail.com>
2026-03-25 14:09:10 -0300

    feat(WorkerPool): created a demo that shows how the worker pool accepts 50 random numbers and returns the squared root

---
+++ go05_Goroutines_Channels/ex02_WorkerPool/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func submitSquareRoot(input int, pool *Pool) {
+	for i := range input {
+		payload := rand.Float64() * 10000
+		job := Job{i, payload}
+		pool.Submit(job)
+	}
+}
+
+func main() {
+	squareRoot := func(j Job) Result {
+		n, ok := j.Payload.(float64)
+		if !ok {
+			return Result{j.ID, 0, fmt.Errorf("the payload is not of type float64")}
+		}
+		res := math.Sqrt(n)
+		return Result{j.ID, res, nil}
+	}
+	pool := NewPool(5, squareRoot)
+
+	go func() {
+		submitSquareRoot(50, pool)
+		pool.Stop()
+	}()
+
+	fmt.Println("### WORKER POOL STARTED ###")
+	for r := range pool.Results() {
+		if r.Err != nil {
+			fmt.Printf("Something went wrong wit ID %d ==> %s\n", r.JobID, r.Err.Error())
+		} else {
+			fmt.Printf("ID: %d; Output: %.2f\n", r.JobID, r.Output)
+		}
+	}
+}
