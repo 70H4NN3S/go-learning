e47a98ea0603cb44489b69dc67b14b25e04c2579
Johannes Niedens <johannesniedens@gmail.com>
2026-03-31 10:36:38 -0300

    LearnGoWithTests Integers added an example to the documentation in the testing file

--- Book_LearnGoWithTests/01_Integers/adder_test.go
+++ Book_LearnGoWithTests/01_Integers/adder_test.go
@@ -1,11 +1,20 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4
 	assertEqual(t, sum, expected)
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
 
 func assertEqual(t testing.TB, sum, expected int) {
