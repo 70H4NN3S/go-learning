b37bc0ee17fe000f329592f7491ff8a47557722a
Johannes Niedens <johannesniedens@gmail.com>
2026-04-02 09:01:13 -0300

    LearnGoWithTests extended the Repeat function

--- Book_LearnGoWithTests/02_Iteration/repeat_test.go
+++ Book_LearnGoWithTests/02_Iteration/repeat_test.go
@@ -1,18 +1,39 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("call once", func(t *testing.T) {
+		got := Repeat("a", 1)
+		want := "a"
+		assertEqual(t, got, want)
+	})
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	t.Run("call five times", func(t *testing.T) {
+		got := Repeat("z", 5)
+		want := "zzzzz"
+		assertEqual(t, got, want)
+	})
+}
+
+func assertEqual(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
+	b.ReportAllocs()
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 50)
 	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("a", 3))
+	// Output: aaa
 }
