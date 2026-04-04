06720f389d0a05839bda5d510416a5ac13c8c091
Johannes Niedens <johannesniedens@gmail.com>
2026-04-04 07:04:25 -0300

    LearnGoWithTests Structs added methods for circle and rectangles

--- Book_LearnGoWithTests/04_Structs/shapes.go
+++ Book_LearnGoWithTests/04_Structs/shapes.go
@@ -1,19 +1,18 @@
 // package shapes handles simple geographic shapes
 package shapes
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rect Rectangle) float64 {
-	width := rect.Width
-	height := rect.Height
-	return 2 * (width + height)
+func (r Rectangle) Perimeter() float64 { return 2 * (r.Width + r.Height) }
+func (r Rectangle) Area() float64      { return r.Width * r.Height }
+
+type Circle struct {
+	Radius float64
 }
 
-func Area(rect Rectangle) float64 {
-	width := rect.Width
-	height := rect.Height
-	return width * height
-}
+func (c Circle) Area() float64 { return math.Pi * c.Radius * c.Radius }
