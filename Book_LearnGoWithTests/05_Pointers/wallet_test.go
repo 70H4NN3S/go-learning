211c6029939fd6a86a9dd7ebda84fc07b133cf5e
Johannes Niedens <johannesniedens@gmail.com>
2026-04-06 12:30:46 -0300

    LearnGoWithTests Pointers added a final error check

--- Book_LearnGoWithTests/05_Pointers/wallet_test.go
+++ Book_LearnGoWithTests/05_Pointers/wallet_test.go
@@ -17,7 +17,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -48,5 +50,12 @@ func assertError(t testing.TB, got, want error) {
 
 	if got.Error() != want.Error() {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Errorf("got an error but didn't wanted one")
 	}
 }
